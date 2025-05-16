818fdc2eed4279c83ceb5935147f57fafd096f4e
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2025-05-17 00:30:56 +0700

    [FIX] fix error load image on error not found page when vercel deployed

--- src/views/errors/ErrorNotFoundLayout.jsx
+++ src/views/errors/ErrorNotFoundLayout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import errorNotFoundImage from "../../assets/page-not-found.svg";
 
 const ErrorNotFoundLayout = () => {
   return (
     <div className="w-full flex flex-col min-h-dvh gap-3 items-center justify-center">
-      <img src="./src/assets/page-not-found.svg" alt="404" />
+      <img src={errorNotFoundImage} alt="404" />
       <h1 className="text-2xl font-bold">Page Not Found</h1>
     </div>
   );
