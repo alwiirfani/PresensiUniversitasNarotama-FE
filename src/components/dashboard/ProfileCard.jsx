4d858212463a288df5209398b94a00bcc68da336
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2025-05-18 03:07:42 +0700

    [UPDATE] add icon up and down when click popover on dashboard

--- src/components/dashboard/ProfileCard.jsx
+++ src/components/dashboard/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import dummyUser from "../../assets/images/user.png";
 import {
   Card,
@@ -8,11 +8,18 @@ import {
   CardTitle,
 } from "../ui/card";
 import { Button } from "../ui/button";
-import { CircleUserRound, KeySquare } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronUp,
+  CircleUserRound,
+  KeySquare,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const ProfileCard = ({ id, nama, role }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Card className="w-[350px] h-[170px] hover:bg-slate-100">
       <CardHeader>
@@ -30,12 +37,13 @@ const ProfileCard = ({ id, nama, role }) => {
         </div>
       </CardHeader>
       <CardFooter className="flex justify-center">
-        <Popover className="w-full">
+        <Popover open={isOpen} onOpenChange={setIsOpen} className="w-full">
           <PopoverTrigger asChild>
             <Button
               className="hover:bg-border border-2 border-border w-full"
               variant="outline">
               {role === "mahasiswa" ? "Mahasiswa" : "Dosen"}
+              {isOpen ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-52 flex flex-col gap-2">
